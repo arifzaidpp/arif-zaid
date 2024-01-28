d5134213c7648605325bd25a5cb5c08c96fb6653
arifzaidpp <arifzaidaiju@gmail.com>
2024-01-28 07:41:58 +0530

    Side Nav Compleated Successfully for Desktop with Profile, Address, nav-lists, footer-icons

---
+++ js/main.js
@@ -0,0 +1,54 @@
+function calculateHeightAndSet() {
+    const div1 = document.getElementById("profile");
+    const div2 = document.getElementById("nav-footer");
+    const body = window.innerHeight;
+    const details = document.getElementById("details");
+    const targetElement = document.getElementById("scrollable");
+
+    const heightTotal = div1.offsetHeight + div2.offsetHeight + details.offsetHeight + 50;
+    const heightDifference = body - heightTotal;
+
+    targetElement.style.height = heightDifference + "px ";
+
+}
+
+calculateHeightAndSet(); // Call the function to perform the calculation and setting
+
+
+function calculateHeightSet() {
+    const div1 = document.getElementById("profile-sm");
+    const div2 = document.getElementById("nav-footer-sm");
+    const body = window.innerHeight;
+    const details = document.getElementById("details-sm");
+    const targetElement = document.getElementById("scrollable-sm");
+
+    const heightTotal = div1.offsetHeight + div2.offsetHeight + details.offsetHeight + 274;
+    const heightDifference = body - heightTotal;
+
+    targetElement.style.height = heightDifference + "px ";
+
+}
+
+calculateHeightSet(); // Call the function to perform the calculation and setting
+
+window.onresize = function () { location.reload(); }
+
+
+const toggler = document.querySelector(".btn");
+toggler?.addEventListener("click", function () {
+    document.querySelector("#nav-list-sm").classList.toggle("collapsed");
+});
+
+$('#shownav').click(function(event) {
+    if ($('.nav-list-sm').hasClass('dismiss')|| $('.nav-list-sm').hasClass('d-none')) {
+      $('.nav-list-sm').removeClass('dismiss').removeClass('d-none').addClass('selected').show();
+    }
+    event.preventDefault();
+  });
+  
+  $('#closenav').click(function(event) {
+    if ($('.nav-list-sm').hasClass('selected')) {
+      $('.nav-list-sm').removeClass('selected').addClass('dismiss');
+    }
+    event.preventDefault();
+  });
