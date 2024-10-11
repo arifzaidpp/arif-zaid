30c3766f99756782055988c646c70bf921446ae7
Arif Zaid PP <arifzaidpp@Arifs-MacBook-Pro.local>
2024-10-11 16:55:05 +0530

    Login completed for all device

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,14 @@
 import { useState } from 'react';
 import './App.css'
 import Home from './pages/home/Home'
+import Login from './pages/login/Login';
 
 function App() {
   const [sidebarOpen, setSidebarOpen] = useState(false);
 
   return (
    <>
-   <Home/>
+   <Login/>
    </>
   )
 }
