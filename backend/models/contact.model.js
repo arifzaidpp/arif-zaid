36f78969b0faf57571fc2f33772baceff17b0ded
Arif Zaid PP <arifzaidpp@Arifs-MacBook-Pro.local>
2024-10-25 09:24:05 +0530

    googl search console html file added

---
+++ backend/models/contact.model.js
@@ -0,0 +1,38 @@
+import mongoose from 'mongoose';
+
+const contactSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    lastName: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    number: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    subject: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    message: {
+        type: String,
+        required: true,
+        trim: true
+    },
+});
+
+const Contact = mongoose.model('Contact', contactSchema);
+
+export default Contact;
